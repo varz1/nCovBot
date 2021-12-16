c5572353b85061b1d2ed70d24b4b67b75c2de234
varz1 <lqcl120638@gmail.com>
2021-12-16 22:48:49 +0800

    test

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"github.com/spf13/viper"
+)
 
+func main() {
+	viper.AddConfigPath("/home/cl/go/src/github.com/varz1/nCovBot/config")
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("viper wrong")
+	}
 }

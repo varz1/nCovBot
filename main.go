0cbdce2a22b4ec0a851db0c32549e1f845a50b5c
varz1 <lqcl120638@gmail.com>
2021-12-26 23:32:51 +0800

    fix

--- main.go
+++ main.go
@@ -5,24 +5,19 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/varz1/nCovBot/bot"
 	"github.com/varz1/nCovBot/maker"
-	"log"
 	"os"
 )
 
 func main() {
-	port := os.Getenv("PORT")
 	app := fiber.New()
 	app.Use(logger.New())
 	go bot.Run()
-	app.Post("/"+os.Getenv("TOKEN"), bot.WebHookHandler)
 	go maker.List()
 	go maker.Overall()
 	go maker.Province()
 	go maker.QueryProvince()
 	go maker.News()
 	go maker.RiskQuery()
-	err2 := app.Listen(":" + port)
-	if err2 != nil {
-		log.Println(err2)
-	}
+	app.Post("/"+os.Getenv("TOKEN"), bot.WebHookHandler)
+	app.Listen(":" + os.Getenv("PORT"))
 }

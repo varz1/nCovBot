4864cee1e4cfc54f66dbf23b63657695b354b030
varz1 <lqcl120638@gmail.com>
2022-01-15 13:08:55 +0800

    中文字体

--- data/yahei.go
+++ data/yahei.go
@@ -8,7 +8,7 @@ import (
 
 func YaHeiFontData() []byte {
 	p,_ := os.Getwd()
-	fontBytes, err := ioutil.ReadFile(p+".fonts/WeiRuanYaHei-1.ttf")
+	fontBytes, err := ioutil.ReadFile(p+"/.fonts/WeiRuanYaHei-1.ttf")
 	if err != nil {
 		log.Println(err)
 		return nil
